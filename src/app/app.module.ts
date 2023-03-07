0a1fac643b91c44119ca4fabf57fd5b16672b53b
IsabelaMaroto <belamaroto@gmail.com>
2023-03-07 11:56:03 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatMenuModule,
+    FormsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+const apiBase = 'https://api.shrtco.de/v2/';
+export interface teste {
+  ok: boolean;
+  result: {
+    full_short_link: string;
+    original_link: string;
+
+  }
+}
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+export class HomeComponent implements OnInit {
+  link = "";
+  resposta: teste[] = []
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+
+  }
+
+  getAPi() {
+    this.http.get<teste>(`${apiBase}shorten?url=${this.link}`).subscribe(teste => {
+      this.resposta.push(teste)
+      console.log(this.resposta)
+    })
+  }
+
+  botao(i: number) {
+    const botao = document.getElementById(`botao${i}`)
+    if (botao) {
+      botao.innerText = 'Copied!'
+    }
+    botao?.classList.add('copied')
+
+    let copyLink= document.getElementById(`link${i}`)?.innerHTML
+    console.log(copyLink)
+    copyLink ? navigator.clipboard.writeText(copyLink) : console.log('erro')
+    
+      
+    }
+  }
